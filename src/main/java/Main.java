d894d21fe8da6afcccbaac05c938c5f14a3d64a8
clement-deriencourt@orange.fr <Parisenforce>
2021-10-21 14:33:25 +0200

    branch clean

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Personnage personne1 = new Personnage("Pierre", 100, 1, "Vivant");
-        Personnage personne2 = new Personnage("Paul", 100, 1, "Vivant");
 
-        System.out.println("Personnage 1 : " + personne1.toString());
-        System.out.println("Personnage 2 : " + personne2.toString());
-
-        personne1.attaque(personne2);
-        System.out.println(personne2.toString());
-        personne1.soigne(personne2);
-        System.out.println(personne2.toString());
     }
 }
